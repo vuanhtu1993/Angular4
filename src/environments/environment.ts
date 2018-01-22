f204b4ad0c5e1241ace021aa259e409fc2243dfc
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-01-22 09:52:22 +0700

    1. Set up environment firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDhD42zHVP8YzELtIf_TTHcmkQbN2pzqAs',
+    authDomain: 'angular4-final.firebaseapp.com',
+    databaseURL: 'https://angular4-final.firebaseio.com',
+    projectId: 'angular4-final',
+    storageBucket: '',
+    messagingSenderId: '107447201422'
+  }
 };
