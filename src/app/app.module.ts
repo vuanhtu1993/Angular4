06debb73ad0289e78984c7ffc18a8577f334f6d3
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-01-22 09:58:52 +0700

    1. Set up environment firebase 2. install angularfire2 $ npm install firebase angularfire2 --save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {}
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
