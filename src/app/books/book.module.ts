b2e050bfa662bb68c5668e049463e8d5278babba
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-01-23 14:22:58 +0700

    19. Finish search real-time feature

--- src/app/books/book.module.ts
+++ src/app/books/book.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {BooksComponent} from './books.component';
 import {BookComponent} from './book/book.component';
 import {BookListComponent} from './book-list/book-list.component';
 import {ManageBookComponent} from './manage-book/manage-book.component';
 import {AppRoutingModule} from '../app-routing.module';
 import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,10 @@ import {ReactiveFormsModule} from '@angular/forms';
     ManageBookComponent,
   ],
   imports: [
+    BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
 })
 export class BookModule {}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/debounce';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-books',
@@ -7,9 +11,39 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  bookSearched = new FormControl();
-  constructor() { }
+  bookForm: FormControl;
+  booksSearched;
+
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
+    this.bookForm = new FormControl();
+    this.bookForm
+      .valueChanges
+      .debounceTime(200)
+      .subscribe((value) => {
+        this.http.get('https://www.googleapis.com/books/v1/volumes?q=' + value)
+          .map((book: any) => {
+           return book.items;
+          })
+          .map((book: Book) => {
+            return book;
+          })
+          .subscribe((book) => {
+            // console.log(books);
+            this.booksSearched = book;
+          });
+      });
   }
+}
+
+export interface Book {
+  title: string;
+  authors: string;
+  publisher: string;
+  description: string;
+  publishedDate: string;
+  pageCount: string;
+  imageLinks: string;
 }
