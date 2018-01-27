2b871d7455851491a10d82d761deb029724f06f7
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-01-28 00:25:26 +0700

    Change environment to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDhD42zHVP8YzELtIf_TTHcmkQbN2pzqAs',
+    authDomain: 'angular4-final.firebaseapp.com',
+    databaseURL: 'https://angular4-final.firebaseio.com',
+    projectId: 'angular4-final',
+    storageBucket: '',
+    messagingSenderId: '107447201422'
+  }
 };
