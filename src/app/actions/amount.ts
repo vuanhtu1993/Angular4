6421cb5e8e086c2dbff0ccc8060a68127bee158f
vuanhtu1993mta@gmail.com <tuntun159753>
2018-02-05 00:28:44 +0700

    1. Create reducer corresponding with 3 actions

--- src/app/actions/amount.ts
+++ src/app/actions/amount.ts
@@ -2,6 +2,7 @@ import {Action} from '@ngrx/store';
 
 export const AMOUNTCHANGE = '[Amount] Change';
 
+// we will also need an action, to report, that the amount was changed by the user.
 export class AmountChangeAction implements Action {
   type = AMOUNTCHANGE;
 
