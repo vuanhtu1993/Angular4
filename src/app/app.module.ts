db2ba06773488459ca48fea15f2311f290a37624
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2018-03-30 20:10:33 +0700

    1. Dont understand navigation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Route, RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { KruskalComponent } from './component/kruskal/kruskal.component';
+import {DijkstraModule} from './dijkstra/dijkstra.module';
+import {KruskalModule} from './kruskal/kruskal.module';
+import {KruskalComponent} from './kruskal/kruskal.component';
+import {DijkstraComponent} from './dijkstra/dijkstra.component';
 
+const routes: Routes = [
+  {path: 'kruskal', component: KruskalComponent},
+  {path: 'dijkstra', component: DijkstraComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    KruskalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DijkstraModule,
+    KruskalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
